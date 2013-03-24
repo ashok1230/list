4802b7025fd30ab08d24d9dcc519a918f0a7b455
ashok1230 <gadiashok0410@gmail.com>
2013-03-25 00:59:09 +0530

    first commit

---
+++ splitting_string_first_last_elements.py
@@ -0,0 +1,26 @@
+class word:
+    def word(self,a):
+	self.word=a
+    def break1(self,wor):
+	brwo=wor.split()
+	return brwo
+    def sort(self,str):
+	brwo=str.split()
+	return sorted(brwo)
+
+class main(word):
+    def method(self,str):
+	self.lst=word.break1(self,str)
+	self.sort=word.sort(self,str)
+	print 'Befoer sorting list is\n',self.lst
+	print 'After sorting list is\n',self.sort
+	last=self.lst[len(self.lst)-1]
+	print 'Before sorting last element is:\n',last
+	first=self.lst[0]
+	print 'Before sorting first element is:\n',first
+	sort_last=self.sort.pop(-1)
+	print 'After sorting last element is:\n',sort_last
+	sort_first=self.sort[0]
+	print 'After sorting first element is:\n',sort_first
+a=main()
+a.method('ashok IV b.Tech sree vidya nikethan')
